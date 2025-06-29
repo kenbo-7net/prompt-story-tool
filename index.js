3b1d9b05259f9d742dd2dc40818be689024d76ae
kenbo-7net <104003646+kenbo-7net@users.noreply.github.com>
2025-06-29 16:06:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('プロンプトストーリーツール、起動完了やで！');
+  res.send('プロンプトストーリーツール、動いてるで！');
 });
 
 app.listen(port, () => {
-  console.log(`サーバーがポート${port}で起動しました`);
+  console.log(`サーバーがポート ${port} で起動中やで`);
 });
