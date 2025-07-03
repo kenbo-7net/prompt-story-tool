7abb7eea999d285c98b62a3230e93a47615a884f
kenbo-7net <104003646+kenbo-7net@users.noreply.github.com>
2025-07-03 23:01:12 +0900

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 
+const historyPath = path.join(__dirname, 'history.json');
+
+function addFeedback(index) {
+  const data = JSON.parse(fs.readFileSync(historyPath));
+  if (data[index]) {
+    data[index].feedback = (data[index].feedback || 0) + 1;
+    fs.writeFileSync(historyPath, JSON.stringify(data, null, 2));
+  }
+}
+
+module.exports = { addFeedback };
